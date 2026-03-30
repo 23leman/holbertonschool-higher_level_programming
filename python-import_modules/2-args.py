17fe271996e4c3e3cfbd96358111b36c8000ecbd
23leman <lemanrakhimli@gmail.com>
2026-03-30 00:59:42 +0000

    Fix PEP 8 W293: remove whitespace from blank lines

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,15 +3,11 @@ import sys
 
 if __name__ == "__main__":
     count = len(sys.argv) - 1
-    
-    # Başlıq hissəsinin çapı
     if count == 0:
         print("0 arguments.")
     elif count == 1:
         print("1 argument:")
     else:
         print("{} arguments:".format(count))
-    
-    # Arqumentlərin siyahısı (əgər varsa)
     for i in range(1, count + 1):
         print("{}: {}".format(i, sys.argv[i]))
