4f46dbcad2f515e1b799e73574f7e60da3e2e9a3
23leman <lemanrakhimli@gmail.com>
2026-03-30 01:00:12 +0000

    Infinite addition with PEP 8 compliance

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,7 +3,6 @@ import sys
 
 if __name__ == "__main__":
     total = 0
-    # Birinci arqument skriptin adıdır, ona görə 1-ci indeksdən başlayırıq
     for i in range(1, len(sys.argv)):
         total += int(sys.argv[i])
     print("{}".format(total))
