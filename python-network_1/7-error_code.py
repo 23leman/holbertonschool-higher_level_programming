d957e3c14e20de9cbeba8b8bd45ffb11115f1d3c
23leman <lemanrakhimli@gmail.com>
2026-04-24 13:54:40 +0000

    Fix pycodestyle E501 and W293 in 7-error_code.py

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Takes in a URL, sends a request to the URL and displays the body of the response.
-If the HTTP status code is >= 400, prints the error code.
+Takes in a URL, sends a request to the URL and displays the body
+of the response.
 """
 import sys
 import requests
@@ -10,7 +10,7 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
     r = requests.get(url)
-    
+
     if r.status_code >= 400:
         print("Error code: {}".format(r.status_code))
     else:
