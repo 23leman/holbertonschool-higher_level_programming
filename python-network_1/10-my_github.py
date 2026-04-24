d51476173decc7652a5569c51f093f295046f76d
23leman <lemanrakhimli@gmail.com>
2026-04-24 13:59:13 +0000

    Fix pycodestyle E501 in 10-my_github.py

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -13,12 +13,9 @@ if __name__ == "__main__":
     user = sys.argv[1]
     token = sys.argv[2]
 
-    # Basic Authentication istifadə edərək GET sorğusu göndəririk
     r = requests.get(url, auth=(user, token))
 
     try:
-        # Cavabı JSON-a çeviririk və 'id' açarını götürürük
-        # Əgər 'id' tapılmazsa (məs. səhv login), .get('id') avtomatik None qaytarır
         print(r.json().get('id'))
     except ValueError:
         print("None")
